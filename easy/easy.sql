e5c6d68bd6dab18a2a3553548f93c0e83353d1dd
Gaurav Sharma <gauraxvx0410@gmail.com>
2025-06-26 17:16:48 +0530

    SELECT

---
+++ easy/easy.sql
@@ -0,0 +1,17 @@
+-- SELECT
+
+
+-- 1. Recyclable and Low Fat Products
+   SELECT product_id FROM Products WHERE low_fats = 'Y' AND recyclable = 'Y';
+
+-- 2. Find Customer Referee
+   SELECT name FROM Customer WHERE referee_id !=2 OR referee_id IS null;
+
+-- 3. Big Countries
+  SELECT name, population, area FROM World WHERE area >= 3000000 OR population >= 25000000;
+
+-- 4. Article Views 1
+  SELECT DISTINCT  author_id AS id FROM Views WHERE author_id = viewer_id ORDER BY author_id;
+
+-- 5. Invalid Tweets
+  SELECT tweet_id FROM Tweets WHERE LENGTH(content) > 15;

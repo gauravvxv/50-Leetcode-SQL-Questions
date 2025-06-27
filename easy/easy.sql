462d18e13d9aa386f9069adbda586e20cf8b4b86
Gaurav Sharma <gauraxvx0410@gmail.com>
2025-06-27 15:30:04 +0530

    customer

--- easy/easy.sql
+++ easy/easy.sql
@@ -23,3 +23,8 @@
 
 --  2. Product Sales Analysis 1
   SELECT p.product_name, s.year, s.price FROM Product p INNER JOIN Sales s ON s.product_id = p.product_id;
+
+--  3. Customer Who Visited but Did Not Make Any Transactions
+  SELECT v.customer_id, count(*) AS count_no_trans FROM Visits v 
+LEFT JOIN Transactions t ON v.visit_id = t.visit_id WHERE t.transaction_id IS NULL
+GROUP BY v.customer_id 
